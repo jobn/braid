ca8f3ada728962fd648e39047b3536f91691fd72
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2018-10-28 21:42:28 +0100

    refactor Story

--- src/Story.js
+++ src/Story.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import Tags from './Tags';
 import Owners from './Owners';
+import { FeatureTag, BugTag, ChoreTag, LabelTag, EstimateTag } from './Tags';
+
+const renderTypeTag = type => {
+  switch (type) {
+    case 'feature':
+      return <FeatureTag />;
+    case 'bug':
+      return <BugTag />;
+    case 'chore':
+      return <ChoreTag />;
+    default:
+      return null;
+  }
+};
 
 const Story = ({ name, labels, story_type, estimate, owner_ids }) => (
   <div className="card">
@@ -9,7 +22,15 @@ const Story = ({ name, labels, story_type, estimate, owner_ids }) => (
 
       <div className="media">
         <div className="media-content">
-          <Tags storyType={story_type} estimate={estimate} labels={labels} />
+          <div className="tags has-addons is-marginless">
+            {renderTypeTag(story_type)}
+
+            <EstimateTag estimate={estimate} />
+
+            {labels.map(label => (
+              <LabelTag name={label.name} key={label.id} />
+            ))}
+          </div>
         </div>
 
         <div className="media-right">
