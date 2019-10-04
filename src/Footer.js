e2c4b2c4078cf16e5acf796f8960193fc50771ba
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2019-10-04 23:51:16 +0200

    add the possibility to have multiple Trays in the Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { createContext, useReducer, useMemo } from 'react';
 
-const Footer = ({ children }) => (
-  <div
-    style={{
-      position: 'fixed',
-      bottom: '0',
-      width: '100vw',
-      display: 'flex',
-      zIndex: '50'
-    }}
-  >
-    {children}
-  </div>
-);
+const FooterContext = createContext();
 
-export { Footer };
+function reducer(_, action) {
+  return action;
+}
+
+const Footer = ({ children }) => {
+  const [openTray, dispatch] = useReducer(reducer, '');
+  const value = useMemo(() => ({ openTray, dispatch }), [openTray, dispatch]);
+
+  return (
+    <FooterContext.Provider value={value}>
+      <div
+        style={{
+          position: 'fixed',
+          bottom: '0',
+          width: '100vw',
+          display: 'flex',
+          zIndex: '50'
+        }}
+      >
+        {children}
+      </div>
+    </FooterContext.Provider>
+  );
+};
+
+export { Footer, FooterContext };
--- src/Tray.js
+++ src/Tray.js
@@ -1,49 +1,56 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { FooterContext } from './Footer';
 
-const trayStyle = {
-  position: 'absolute',
-  width: '100vw',
-  backgroundColor: 'white',
-  transition: 'transform 0.3s cubic-bezier(.65,.69,.77,1.25)',
-  padding: '1rem',
-  boxShadow: '0 -2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1)'
-};
+function getTrayStyle(fullWidth, open) {
+  return {
+    position: 'absolute',
+    width: fullWidth ? '100vw' : '100%',
+    backgroundColor: 'white',
+    transition: 'transform 0.3s cubic-bezier(.65,.69,.77,1.25)',
+    padding: '1rem',
+    boxShadow: '0 -2px 3px rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.1)',
+    transform: open ? 'translateY(-100%)' : 'translateY(0)'
+  };
+}
 
-const Tray = ({ title, children, renderLabel }) => {
-  const [open, setOpen] = useState(false);
+const Tray = ({ title, rightAlign, fullWidth, children, renderLabel }) => {
+  const { openTray, dispatch } = useContext(FooterContext);
+  const open = openTray === title;
 
   return (
-    <div style={{ flex: '1 1 0%' }}>
-      <div
-        data-testid="tray"
-        style={{
-          ...trayStyle,
-          transform: open ? 'translateY(-100%)' : 'translateY(0)'
-        }}
-      >
+    <div className="is-relative" style={{ flex: '1 1 0%' }}>
+      <div data-testid="tray" style={getTrayStyle(fullWidth, open)}>
         {children}
       </div>
 
       <button
-        style={{ justifyContent: 'flex-start' }}
+        style={{ justifyContent: rightAlign ? 'flex-end' : 'flex-start' }}
         className="button is-fullwidth is-dark is-medium is-radiusless"
-        onClick={() => setOpen(!open)}
+        onClick={() => dispatch(open ? '' : title)}
       >
+        {!open && rightAlign && (
+          <div style={{ marginLeft: '.5rem' }}>{renderLabel()}</div>
+        )}
+
         <span>{title}</span>
 
         <span className="icon is-small" style={{ margin: '0' }}>
           <FontAwesomeIcon icon={open ? faAngleDown : faAngleUp} />
         </span>
 
-        {!open && <div style={{ marginLeft: '.5rem' }}>{renderLabel()}</div>}
+        {!open && !rightAlign && (
+          <div style={{ marginLeft: '.5rem' }}>{renderLabel()}</div>
+        )}
       </button>
     </div>
   );
 };
 
 Tray.defaultProps = {
+  rightAlign: false,
+  fullWidth: false,
   renderLabel: () => {}
 };
 
