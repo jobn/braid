be34a86aaf7da4d5469328f4a602b7beda3a5aeb
Cilia Vandenameele <cilia@yesplan.be>
2022-11-17 13:33:17 +0100

    updated: use labels for settings so checkboxes are more easy to toggle

--- src/Settings.js
+++ src/Settings.js
@@ -22,6 +22,7 @@ const Settings = () => {
         <div className="media-left">
           <label className="switch">
             <input
+              id="split-final-columns-setting"
               type="checkbox"
               checked={splitFinalColumns ? false : 'checked'}
               onChange={() => setSplitFinalColumns(!splitFinalColumns)}
@@ -31,10 +32,13 @@ const Settings = () => {
         </div>
 
         <div className="media-content content" style={{ marginRight: '1rem' }}>
-          <h4>
-            Combine &ldquo;Delivered&rdquo; and &ldquo;Accepted&rdquo; columns
-            into one
-          </h4>
+          <label htmlFor="split-final-columns-setting">
+            <h4>
+              Combine &ldquo;Delivered&rdquo; and &ldquo;Accepted&rdquo; columns
+              into one
+            </h4>
+          </label>
+
           <p>
             Enable this setting to have the Delivered and Accepted story states
             represented as a single column in the view.
@@ -49,6 +53,7 @@ const Settings = () => {
           <label className="switch">
             <input
               type="checkbox"
+              id="split-finished-delivered-setting"
               checked={splitFinishedAndDelivered ? false : 'checked'}
               onChange={() =>
                 setSplitFinishedAndDelivered(!splitFinishedAndDelivered)
@@ -59,10 +64,13 @@ const Settings = () => {
         </div>
 
         <div className="media-content content" style={{ marginRight: '1rem' }}>
-          <h4>
-            Combine &ldquo;Finished&rdquo; and &ldquo;Delivered&rdquo; columns
-            into one
-          </h4>
+          <label htmlFor="split-finished-delivered-setting">
+            <h4>
+              Combine &ldquo;Finished&rdquo; and &ldquo;Delivered&rdquo; columns
+              into one
+            </h4>
+          </label>
+
           <p>
             Enable this setting to have the Finished and Delivered story states
             represented as a single column in the view.
@@ -78,6 +86,7 @@ const Settings = () => {
           <label className="switch">
             <input
               type="checkbox"
+              id="slim-theme-setting"
               checked={slim ? 'checked' : false}
               onChange={() => setSlim(!slim)}
             />
@@ -86,9 +95,11 @@ const Settings = () => {
         </div>
 
         <div className="media-content content" style={{ marginRight: '1rem' }}>
-          <h4>
-            <span className="is-underline">S</span>lim theme
-          </h4>
+          <label htmlFor="slim-theme-setting">
+            <h4>
+              <span className="is-underline">S</span>lim theme
+            </h4>
+          </label>
           <p>
             Enable this setting to have each story take up less space on screen,
             and see more stories at a time.
@@ -103,6 +114,7 @@ const Settings = () => {
           <label className="switch">
             <input
               type="checkbox"
+              id="use-full-names-setting"
               checked={useFullNames ? 'checked' : false}
               onChange={() => setUseFullNames(!useFullNames)}
             />
@@ -110,11 +122,15 @@ const Settings = () => {
           </label>
         </div>
         <div className="media-content content" style={{ marginRight: '1rem' }}>
-          <h4>
-            <span className="is-underline">F</span>ull names
-          </h4>
+          <label htmlFor="use-full-names-setting">
+            <h4>
+              <span className="is-underline">F</span>ull names
+            </h4>
+          </label>
+
           <p>
-            Enable this setting to have the names in the navbar be displayed as full names instead of initials
+            Enable this setting to have the names in the navbar be displayed as
+            full names instead of initials
             <br />
             Current state: <b>{useFullNames ? 'enabled' : 'disabled'}</b>
           </p>
@@ -125,6 +141,7 @@ const Settings = () => {
         <div className="media-left">
           <label className="switch">
             <input
+              id="show-labels-setting"
               type="checkbox"
               checked={showLabels ? 'checked' : false}
               onChange={() => setShowLabels(!showLabels)}
@@ -134,9 +151,12 @@ const Settings = () => {
         </div>
 
         <div className="media-content content" style={{ marginRight: '1rem' }}>
-          <h4>
-            <span className="is-underline">S</span>how labels in extended view
-          </h4>
+          <label htmlFor="show-labels-setting">
+            <h4>
+              <span className="is-underline">S</span>how labels in extended view
+            </h4>
+          </label>
+
           <p>
             Enable this setting to have each story take up less space on screen
             in the extended view, and see more stories at a time by hiding the
