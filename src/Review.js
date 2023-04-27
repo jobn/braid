c65bd8454ae84ed3d3577ba5d3796be8288a718f
Laure Philips <laure@yesplan.be>
2023-04-27 14:00:59 +0200

    extend Review component with option to show initials of the reviewer/requester

--- src/Review.js
+++ src/Review.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import { Initials } from './Initials';
 
 const colorForReview = status => {
   switch (status) {
@@ -8,12 +9,14 @@ const colorForReview = status => {
       return 'Red';
     case 'in_review':
       return 'Orange';
+    case 'pass':
+      return 'LightGreen';
     default:
       return 'White';
   }
 };
 
-const Review = ({ reviews, slim }) => (
+const Review = ({ reviews, slim, showReviewer, requester }) => (
   <div className="tags has-addons is-marginless" data-testid="reviews">
     {reviews.map(review => (
       <div key={review.id} style={{}}>
@@ -42,10 +45,24 @@ const Review = ({ reviews, slim }) => (
             paddingLeft: slim ? '.5em' : '.75em',
             paddingRight: slim ? '.5em' : '.75em'}}>
           {review.reviewType.name}
+          {showReviewer && (
+            <div style={{ marginLeft: '0.1rem' }}>
+              (
+              <Initials id={review.reviewerId} />)
+            </div>
+          )}
         </div>
       </div>
     ))}
-    {reviews.length === 0 && <div className="tag">Requester</div>}
+    {reviews.length === 0 && (
+      <div className="tag">
+        Requester
+        <div style={{ marginLeft: '0.1rem' }}>
+          (
+          <Initials id={requester} />)
+        </div>
+      </div>
+    )}
   </div>
 );
 
